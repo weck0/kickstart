f436a805c8c27e1c327e7ac9d1bbe87a04642196
Teru <uneteru@protonmail.com>
2021-12-11 13:57:35 +0100

    adding new contract address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0x1De538B5ad10E74e5fAcb84DFE818c0C76FCffab'
+    '0xbEF81D3B9cb6A219f2CF08C49717292bdE33C777'
 );
 
 export default instance;
