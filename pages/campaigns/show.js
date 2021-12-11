2ccaeff79773b31375f5b103f55ab138521b3239
Teru <uneteru@protonmail.com>
2021-12-11 18:37:42 +0100

    adding campaign details

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
+
+    static async getInitialProps(props) {
+        const campaign = Campaign(props.query.address);
+        const summary = await campaign.methods.getSummary().call();
+        return { 
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+         };
+    }
+    
+    renderCards() {
+        const {
+            balance,
+            manager,
+            minimumContribution,
+            requestsCount,
+            approversCount
+        } = this.props;
+
+        const items = [
+            {
+              header: manager,
+              description: 'The manager which created this campaign and can create requests to withdraw money.',
+              meta: 'Manager Address',
+              style: { overflowWrap: 'break-word'}
+            }]
+
+            return <Card.Group items={items} />;
+    }
+
     render() {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
+                {this.renderCards()}
             </Layout>
         );
     }
